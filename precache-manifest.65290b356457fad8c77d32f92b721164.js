ecadf3d0e3076de6498acb7cb796de4f02d23b08
Uzair <uzair.farooqui@technado.ltd>
2020-02-26 04:31:08 +0500

    Updates

---
+++ precache-manifest.65290b356457fad8c77d32f92b721164.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36c24ef237a96ea6f82b9c1506466052",
+    "url": "/reactapps/index.html"
+  },
+  {
+    "revision": "cc8c5b443dc0d521d2f7",
+    "url": "/reactapps/static/css/main.7556c468.chunk.css"
+  },
+  {
+    "revision": "bef45c726e1e85f8485e",
+    "url": "/reactapps/static/js/2.87018454.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/reactapps/static/js/2.87018454.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc8c5b443dc0d521d2f7",
+    "url": "/reactapps/static/js/main.dd53bce7.chunk.js"
+  },
+  {
+    "revision": "aa0762f90cbb03a2a4bc",
+    "url": "/reactapps/static/js/runtime-main.4a29b61f.js"
+  }
+]);
